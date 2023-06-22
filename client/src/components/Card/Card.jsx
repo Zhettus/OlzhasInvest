98292e8ad7300806a298e15868d938bfd4542054
Zhadyra <t_zhadyra_t@mail.ru>
2023-06-22 15:17:13 +0600

    cards improved

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import "./Card.css";
 import {useState} from "react";
 import { motion, AnimateSharedLayout  } from "framer-motion";
@@ -15,6 +15,43 @@ const Card = ({emoji, heading, detail, color}) => {
         <button className="c-button">Узнать больше</button>
       </motion.div>
       
+    </div>
+  );
+};
+
+export default Card;*/
+
+import React, { useState } from "react";
+import "./Card.css";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Card = ({ emoji, heading, detail, color }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCard = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`card ${isOpen ? "open" : ""}`} style={{ borderColor: color }}>
+      <img src={emoji} alt="" />
+      <span>{heading}</span>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.span
+            key="detail"
+            className="card-detail"
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0 }}
+          >
+            {detail}
+          </motion.span>
+        )}
+      </AnimatePresence>
+      <motion.div className="card-button" onClick={toggleCard}>
+        <button className="c-button">{isOpen ? "Скрыть" : "Узнать больше"}</button>
+      </motion.div>
     </div>
   );
 };
